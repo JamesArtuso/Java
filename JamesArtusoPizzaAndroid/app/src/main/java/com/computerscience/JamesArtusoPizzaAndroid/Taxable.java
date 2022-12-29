4ff514a66b734bb2df0df99ab9905ab0f6571ce8
JamesArtuso <121574218+JamesArtuso@users.noreply.github.com>
2022-12-28 19:30:42 -0500

    Add files via upload

---
+++ JamesArtusoPizzaAndroid/app/src/main/java/com/computerscience/JamesArtusoPizzaAndroid/Taxable.java
@@ -0,0 +1,8 @@
+package com.computerscience.JamesArtusoPizzaAndroid;
+/**
+ * Interface for taxable items
+ * @author James Artuso
+ */
+public interface Taxable {
+    void setPrices();
+}
---
+++ JamesArtusoPizzaAndroid/app/src/main/java/com/computerscience/JamesArtusoPizzaAndroid/Titleable.java
@@ -0,0 +1,9 @@
+package com.computerscience.JamesArtusoPizzaAndroid;
+
+/**
+ * Interface for titleable items
+ * @author James Artuso
+ */
+public interface Titleable {
+    String getTitle();
+}
